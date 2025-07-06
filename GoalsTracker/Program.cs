9d26be5304244b78717981f58d37d57c0191d8bd
michaelzrork <michaelzrork@gmail.com>
2025-07-06 13:19:17 -0400

    Switch database from SQLite to SQL Server
    Updated `AddDbContext` to use SQL Server for production.
    Added migration application before the app starts to ensure the database schema is up to date.

--- GoalsTracker/Program.cs
+++ GoalsTracker/Program.cs
@@ -5,11 +5,20 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
+// Use SQL Server instead of SQLite in production
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
-            .LogTo(Console.WriteLine, LogLevel.Information));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .LogTo(Console.WriteLine, LogLevel.Information));
 
 var app = builder.Build();
+
+// Apply migrations before the app starts handling requests
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();  // This will create the tables if they don't exist
+}
 
 if (!app.Environment.IsDevelopment())
 {
@@ -24,3 +33,4 @@ app.UseRouting();
 app.MapRazorPages();
 
 app.Run();
+
